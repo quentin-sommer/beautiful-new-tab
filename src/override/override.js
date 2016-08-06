a400dc80449a72d7ab156c330e39b100bbbcffb4
Quentin Sommer <sommerquentin67@gmail.com>
2016-08-06 20:18:13 +0200

    removed jquery

--- src/override/override.js
+++ src/override/override.js
@@ -1,4 +1,17 @@
-$(function () {
+function fadeIn(el, display) {
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
   const unsplash = new Unsplash.default({
     applicationId: "d39a01d0afa7b51ca661ea6ebab52603bd76777e42427dd9f7c2cd5a6dc90398",
     secret: "cf53c6b76854ea12934c56c145a85c3ede4f04c66eac028a0bcf526af331c111",
@@ -16,10 +29,10 @@ $(function () {
       .then(res => {
         const img = new Image();
         img.onload = function () {
-          const container = $('#photoContainer');
-          container.css('background', 'url(' + res.urls[imgPath] + ') no-repeat center center fixed');
-          container.css('background-size', 'cover');
-          container.fadeIn(500);
+          const photoContainer = document.getElementById('photoContainer');
+          photoContainer.style.background = 'url(' + res.urls[imgPath] + ') no-repeat center center fixed';
+          photoContainer.style.backgroundSize = 'cover';
+          fadeIn(photoContainer, 'block');
         };
 
         img.src = res.urls[imgPath];
